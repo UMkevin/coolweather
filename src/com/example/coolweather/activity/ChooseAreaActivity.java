0fc93a9aa73afbd1089e9e4bcdf5df4ec25baf1a
Kevin <gekai02090815@126.com>
2015-10-15 17:20:23 +0800

    加入显示天气信息的功能

--- src/com/example/coolweather/activity/ChooseAreaActivity.java
+++ src/com/example/coolweather/activity/ChooseAreaActivity.java
@@ -72,14 +72,14 @@ public class ChooseAreaActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	/*	isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity) {
 			Intent intent = new Intent(this, WeatherActivity.class);
 			startActivity(intent);
 			finish();
 			return;
-		} */
+		} 
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -97,13 +97,13 @@ public class ChooseAreaActivity extends Activity {
 				} else if (currentLevel == LEVEL_CITY) {
 					selectedCity = cityList.get(index);
 					queryCounties();
-				}  /*else if (currentLevel == LEVEL_COUNTY) {
+				}  else if (currentLevel == LEVEL_COUNTY) {
 					String countyCode = countyList.get(index).getCountyCode();
 					Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
 					intent.putExtra("county_code", countyCode);
 					startActivity(intent);
 					finish();
-				}*/
+				}
 			}
 		});
 		queryProvinces();  // 加载省级数据
