1f3f5e114bbb7ed140257045da51b1c4e25af553
Kevin <gekai02090815@126.com>
2015-10-15 16:38:30 +0800

    完成遍历省市县三级列表的功能.

--- src/com/example/coolweather/db/CoolWeatherOpenHelper.java
+++ src/com/example/coolweather/db/CoolWeatherOpenHelper.java
@@ -8,23 +8,23 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 
 	// provice 建表语句
-	public static final String CREATE_PROVINCE = "create table Province{"
+	public static final String CREATE_PROVINCE = "create table Province("
 			+ " id integer primary key autoincrement, "
 			+ " province_name text, "
 			+ " province_code text)";
 	
 	// City建表语句
-		public static final String CREATE_CITY = "create table City{"
+		public static final String CREATE_CITY = "create table City("
 				+ " id integer primary key autoincrement, "
 				+ " city_name text, "
 				+ " city_code text, "
 				+ " province_id integer)";
 		
 		// Country建表语句
-				public static final String CREATE_COUNTRY = "create table Country{"
+				public static final String CREATE_COUNTY = "create table County("
 						+ " id integer primary key autoincrement, "
-						+ " country_name text, "
-						+ " country_code text, "
+						+ " county_name text, "
+						+ " county_code text, "
 						+ " city_id integer)";
 				
 				public CoolWeatherOpenHelper(Context context, String name, CursorFactory factory, int version){
@@ -35,12 +35,12 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_PROVINCE);
 		db.execSQL(CREATE_CITY);
-		db.execSQL(CREATE_COUNTRY);
+		db.execSQL(CREATE_COUNTY);
 
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int oldVersion, int newVersion) {
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
 
 	}
---
+++ src/com/example/coolweather/util/HttpCallbackListener.java
@@ -0,0 +1,10 @@
+package com.example.coolweather.util;
+
+public interface HttpCallbackListener {
+
+
+	void onFinish(String response);
+
+	void onError(Exception e);
+
+}
